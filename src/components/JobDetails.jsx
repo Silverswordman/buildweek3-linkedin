7679cfe0fba58ce948c9ff4643e1e04f4fd320e1
Giulia Silvestrini <silvergiuly@hotmail.it>
2023-11-15 11:26:32 +0100

    parse html

--- src/components/JobDetails.jsx
+++ src/components/JobDetails.jsx
@@ -1,32 +1,27 @@
-import { Card, Col, Container, Row } from "react-bootstrap"
+import { Card, Col, Container, Row } from "react-bootstrap";
+import parse from "html-react-parser";
 
-
-const JobDetails = (props)=>{
-    return (
+const JobDetails = (props) => {
+  return (
     <Container className="mt-4">
-        <Card>
-            <Row>
-                <Col >
-                <h4 className="mt-2 ms-2">Job Details</h4>
-                </Col>
-                
-            </Row>
-        </Card>
-        <Card className="mt-2">
-            <Row className="row-cols-1">
-               
-                <Col>
-                <h3>{props.jobDetails.company_name}</h3>
-                <p>{props.jobDetails.title}</p>
-                </Col>
-                <Col>
-                <p>{props.jobDetails.description}</p>
-                </Col>
-            </Row>
-        </Card>
+      <Card>
+        <Row>
+          <Col>
+            <h4 className="mt-2 ms-2">Job Details</h4>
+          </Col>
+        </Row>
+      </Card>
+      <Card className="mt-2">
+        <Row className="row-cols-1">
+          <Col>
+            <h3>{props.jobDetails.company_name}</h3>
+            <p>{props.jobDetails.title}</p>
+          </Col>
+          <Col>{parse(props.jobDetails.description)}</Col>
+        </Row>
+      </Card>
     </Container>
-        )
+  );
+};
 
-}
-
-export default JobDetails
+export default JobDetails;
--- src/components/PersonalProfile.jsx
+++ src/components/PersonalProfile.jsx
@@ -1,48 +1,65 @@
-import { Button, Card, Col, Container, Row } from "react-bootstrap"
-import { useSelector } from "react-redux"
-import {HiOutlinePencil} from 'react-icons/hi'
-import { Link, useLocation } from "react-router-dom"
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { HiOutlinePencil } from "react-icons/hi";
+import { Link, useLocation } from "react-router-dom";
 
-const PersonalProfile = ()=>{
+const PersonalProfile = () => {
+  const dataProfile = useSelector((state) => state.profile.profile);
+  const { pathname } = useLocation();
+  // console.log(pathname)
+  const isMeRoute = pathname === "/me";
 
-    const dataProfile = useSelector(state=> state.profile.profile)
-    const {pathname} = useLocation()
-    // console.log(pathname)
-    const isMeRoute = pathname === "/me"
-
-    return (
-      <Card className="mt-4 p-3">
-        <Container>
-          {dataProfile && Object.keys(dataProfile).length > 0 ? (
-            <Row>
-            
-              <Col md={12}>
-                <div>
-                  <img src={dataProfile.image} alt="profile-img" width={150} className="rounded-circle" />
-                </div>
-              </Col>
-              <Col md={12} className="mt-3">
-                <div className="d-flex justify-content-between align-items-center">
+  return (
+    <Card className="mt-4 p-3">
+      <Container>
+        {dataProfile && Object.keys(dataProfile).length > 0 ? (
+          <Row>
+            <Col md={12}>
+              <div>
+                <img
+                  src={dataProfile.image}
+                  alt="profile-img"
+                  width={150}
+                  className="rounded-circle"
+                />
+              </div>
+            </Col>
+            <Col md={12} className="mt-3">
+              <div className="d-flex justify-content-between align-items-center">
                 <h2>{`${dataProfile.name} ${dataProfile.surname}`}</h2>
                 {isMeRoute && (
-                    <Link to="/settingsprofile" className="text-decoration-none">
-                      <HiOutlinePencil style={{ fontSize: '1.5em', cursor: 'pointer' }} />
-                    </Link>
-                  )}
-                </div>
-                <p className="m-0">{dataProfile.title}</p>
-                <p className="m-0 text-secondary">{dataProfile.area}</p>     
-              </Col>
-              <Col md={12}>
-                <Button variant="primary" className="rounded-5 fw-bold me-3 mt-3">Disponibile per</Button>
-                <Button variant="outline-primary" className="rounded-5 fw-bold me-3 mt-3">Aggiungi sezione profilo</Button>
-                <Button variant="outline-secondary" className="rounded-5 me-3 mt-3">Altro</Button>
-              </Col>
-            </Row>
-          ) : null}
-        </Container>
-      </Card>
-        )
-}
+                  <Link to="/settingsprofile" className="text-decoration-none">
+                    <HiOutlinePencil
+                      style={{ fontSize: "1.5em", cursor: "pointer" }}
+                    />
+                  </Link>
+                )}
+              </div>
+              <p className="m-0">{dataProfile.title}</p>
+              <p className="m-0 text-secondary">{dataProfile.area}</p>
+            </Col>
+            <Col md={12}>
+              <Button variant="primary" className="rounded-5 fw-bold me-3 mt-3">
+                Disponibile per
+              </Button>
+              <Button
+                variant="outline-primary"
+                className="rounded-5 fw-bold me-3 mt-3"
+              >
+                Aggiungi sezione profilo
+              </Button>
+              <Button
+                variant="outline-secondary"
+                className="rounded-5 me-3 mt-3"
+              >
+                Altro
+              </Button>
+            </Col>
+          </Row>
+        ) : null}
+      </Container>
+    </Card>
+  );
+};
 
 export default PersonalProfile;
